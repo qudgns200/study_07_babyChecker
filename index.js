0cefc5162e148e6103e41be229cc613b069c8140
lee byeonghoon <qudgns200@gmail.com>
2022-08-30 20:58:54 +0900

    modify index.js

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// index Page
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+// feeding Page
+app.get('/feeding', (req, res) => {
+  res.send('This page is for feeding!')
+})
+
+// pee&pooh Page
+app.get('/peePooh', (req, res) => {
+  res.send('This page is for peePooh!')
+})
+
+// sleeping Page
+app.get('/sleeping', (req, res) => {
+  res.send('This page is for sleeping!')
 })
 
 app.listen(port, () => {
