d2703ee7917f619043a17fa17870d79fc1e4bd37
lee byeonghoon <qudgns200@gmail.com>
2022-09-01 09:10:48 +0900

    modify project

--- server/app.js
+++ server/app.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
+const mysql = require('mysql');
+const dbconfig = require('./db.js');
+const connection = mysql.createConnection(dbconfig);
 
 app.get('/', (req, res) =>
     res.send('Hello World!')
 );
 
-app.listen(port, () => 
-    console.log('Example app listening on port ${port}!')
+app.get('/test', (req, res) =>
+    connection.query('SELECT * from information', (error, rows, fields) => {
+    if (error) throw error;
+    console.log('information is: ', rows);
+    res.send(rows);
+  })
 );
 
+app.listen(port, () => 
+    console.log('Express server listening on port ' + app.get('port'))
+);
